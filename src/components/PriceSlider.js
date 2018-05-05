50967731118c09e0f4664f1ca0ab0d2b96f70e95
Jean Philip de Rogatis <jrogatis@metaconexao.com.br>
2018-05-05 16:00:32 -0300

    feat: add price range

--- src/components/PriceSlider.js
+++ src/components/PriceSlider.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import Slider from 'rc-slider';
 import Typography from 'material-ui/Typography';
 import Grid from 'material-ui/Grid';
+import TextField from 'material-ui/TextField';
 import { withStyles } from 'material-ui/styles';
 import { connect } from 'react-redux';
 import 'rc-slider/assets/index.css';
@@ -13,17 +14,23 @@ const styles = theme => ({
     margin: 20,
     width: 200,
   },
+  textField: {
+    marginLeft: theme.spacing.unit,
+    marginRight: theme.spacing.unit,
+    width: 50,
+  },
 });
 
 class PriceSlider extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      min: 100,
-      max: 800,
+      min: Math.round(props.hotels.minPrice) || 100,
+      max: Math.round(props.hotels.maxPrice) || 800,
     };
   }
   onMinChange = e => {
+    console.log('opa');
     this.setState({
       min: +e.target.value || 100,
     });
@@ -31,27 +38,39 @@ class PriceSlider extends Component {
 
   onMaxChange = e => {
     this.setState({
-      max: +e.target.value || 800,
+      max: +e.target.value || this.state.max,
     });
   };
 
+  handleChange(minMax) {
+    this.setState({ min: minMax[0], max: minMax[1] });
+  }
+
   render() {
-    const { classes } = this.props;
+    const { classes, hotels } = this.props;
+    const { min, max } = this.state;
     return (
       <div>
         <Typography align="center"> Price Range per night </Typography>
         <Range
           className={classes.slider}
-          onChange={this.handleChange}
-          min={this.state.min}
-          max={this.state.max}
+          onChange={minMax => this.handleChange(minMax)}
+          min={hotels.minPrice}
+          max={hotels.maxPrice}
           allowCross={false}
           trackStyle={[{ backgroundColor: '#F98100' }, { backgroundColor: '#F98100' }]}
-          handleStyle={[{ backgroundColor: '#F98100' }, { backgroundColor: '#F98100' }]}
+          handleStyle={[
+            { backgroundColor: '#F98100', borderColor: '#F98100' },
+            { backgroundColor: '#F98100', borderColor: '#F98100' },
+          ]}
           railStyle={{ backgroundColor: '#F98100' }}
-          step={100}
-          defaultValue={[200, 600]}
+          step={1}
+          defaultValue={[hotels.minPrice, hotels.maxPrice]}
         />
+        <Grid container direction="row" justify="space-between">
+          <TextField label="Min" className={classes.textField} value={min} />
+          <TextField label="Max" className={classes.textField} value={max} />
+        </Grid>
       </div>
     );
   }
